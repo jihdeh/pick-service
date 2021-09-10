ece53896fbbf0ab8c03f35a501aac55fd76f4156
Babajide Fowotade <jide.b.tade@gmail.com>
2021-09-10 02:22:20 +0100

    chore: remove redis reference due to time constraint

--- configuration.ts
+++ configuration.ts
@@ -1,23 +1,20 @@
-import dotenv from "dotenv";
-import Joi from "joi";
-import path from "path";
+import dotenv from 'dotenv';
+import Joi from 'joi';
+import path from 'path';
 
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
-    ENV: Joi.string()
-      .valid("production", "development", "staging", "test")
-      .required(),
+    ENV: Joi.string().valid('production', 'development', 'staging', 'test').required(),
     PORT: Joi.number().default(3000),
-    DEV_DATABASE_URL: Joi.string().required().description("Dev database url"),
-    DATABASE_URL: Joi.string().required().description("database url"),
-    REDIS_PUBLIC_URL: Joi.string().required().description("redis public url"),
+    DEV_DATABASE_URL: Joi.string().required().description('Dev database url'),
+    DATABASE_URL: Joi.string().required().description('database url'),
   })
   .unknown();
 
 const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
+  .prefs({ errors: { label: 'key' } })
   .validate(process.env);
 
 if (error) {
@@ -29,7 +26,6 @@ const envs = {
   port: envVars.PORT,
   devDatabaseUrl: envVars.DEV_DATABASE_URL,
   prodDatabaseUrl: envVars.DATABASE_URL,
-  redisPublicUrl: envVars.REDIS_PUBLIC_URL,
 };
 
 export default envs;
